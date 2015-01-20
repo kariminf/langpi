d353eeadfdc953d4aa775a3eb3e49da042980dd3
Abdelkrime Aries <kariminfo0@gmail.com>
2015-01-20 13:58:32 +0100

    add Info for arabic and English

---
+++ src/aak/as/preProcess/english/EnInfo.java
@@ -0,0 +1,27 @@
+package aak.as.preProcess.english;
+
+import aak.as.preProcess.lang.PreProcessInfo;;
+
+public class EnInfo implements PreProcessInfo {
+
+	@Override
+	public String getISO639_1() {
+		return "en";
+	}
+
+	@Override
+	public String getLangEnglishName() {
+		return "English";
+	}
+
+	@Override
+	public String getLangName() {
+		return "English";
+	}
+
+	@Override
+	public String getPrefix() {
+		return "En";
+	}
+
+}
