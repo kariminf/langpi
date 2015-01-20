56d9088eab8b9c3dffe17fe614b256a167bb318c
Abdelkrime Aries <kariminfo0@gmail.com>
2015-01-20 11:18:10 +0100

    adding Czech plugin

---
+++ CsPreProcess/src/aak/as/preProcess/czech/CsStemmer.java
@@ -0,0 +1,53 @@
+
+package aak.as.preProcess.czech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.tartarus.snowball.ext.czechStemmer;
+
+import aak.as.preProcess.lang.Stemmer;
+
+public class CsStemmer implements Stemmer {
+
+	@Override
+	public List<String> stemListWords(List<String> listWords) {
+
+		List<String> stemmedLW = new ArrayList<String>();
+		
+		for (String word: listWords)
+			stemmedLW.add(stemWord(word));
+		
+		return stemmedLW;
+	}
+	
+	
+	public static String stemWord (String word){
+		
+		czechStemmer stemmer = new czechStemmer();
+		
+		stemmer.setCurrent(word);
+		stemmer.stem();
+		return stemmer.getCurrent();
+	}
+	
+	public static void main(String[] args) {
+		CsStemmer stemmer=new CsStemmer();
+		List<String> lst = new ArrayList<String>();
+		lst.add("sebevražedný");
+		lst.add("londýnského");
+		lst.add("právníka");
+		lst.add("odposlechy");
+	
+		lst = stemmer.stemListWords(lst);
+		System.out.println(lst.toString());	
+
+	}
+	
+	
+}
+
+
+
+
+
