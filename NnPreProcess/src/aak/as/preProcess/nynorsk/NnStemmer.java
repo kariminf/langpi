83f9fc45221c352aac5d09ae8b0e5715aab2fba7
Abdelkrime Aries <kariminfo0@gmail.com>
2015-01-20 11:28:49 +0100

    adding Norwegian Nynorsk  plugin

---
+++ NnPreProcess/src/aak/as/preProcess/nynorsk/NnStemmer.java
@@ -0,0 +1,70 @@
+
+package aak.as.preProcess.nynorsk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.no.NorwegianLightStemmer;
+
+import aak.as.preProcess.lang.Stemmer;
+
+public class NnStemmer implements Stemmer {
+
+	@Override
+	public List<String> stemListWords(List<String> listWords) {
+
+		List<String> stemmedLW = new ArrayList<String>();
+		
+		for (String word: listWords)
+			stemmedLW.add(stemWord(word));
+		
+		return stemmedLW;
+	}
+	
+	
+	public static String stemWord (String word){
+		
+		
+		char[] wordseq = word.toCharArray();
+		int newlength = word.length();
+		{
+			//remove Bokmål-specific endings = 1
+			//remove Nynorsk-specific endings = 2
+			NorwegianLightStemmer stemmer = new NorwegianLightStemmer(2);
+			newlength = stemmer.stem(wordseq, newlength);
+		}
+		
+		char[] newwordseq = new char[newlength];
+		System.arraycopy(wordseq, 0, newwordseq, 0, newlength);
+
+		return new String(newwordseq);
+	}
+	
+	public static void main(String[] args) {
+		Stemmer Stemmer=new NnStemmer();
+		List<String> tstList = new ArrayList<String>();
+		tstList.add("fitjar");
+		tstList.add("kommune");
+		tstList.add("består");
+		tstList.add("den");
+		tstList.add("nordlege");
+		tstList.add("halvdelen");
+		tstList.add("øya");
+		tstList.add("stord");
+		tstList.add("fitjarøyane");
+		tstList.add("mellom");
+		tstList.add("stord");
+		tstList.add("bømlo");
+	
+		tstList = Stemmer.stemListWords(tstList);
+		System.out.println(tstList.toString());	
+
+	}
+	
+	
+}
+
+
+
+
+
