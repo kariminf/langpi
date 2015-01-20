847cd5c4c721f720dab2e9b24cc12af5f20e7506
Abdelkrime Aries <kariminfo0@gmail.com>
2015-01-20 11:17:43 +0100

    adding Catalan plugin

---
+++ CaPreProcess/src/aak/as/preProcess/catalan/CaStemmer.java
@@ -0,0 +1,70 @@
+
+package aak.as.preProcess.catalan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.tartarus.snowball.ext.CatalanStemmer;
+
+import aak.as.preProcess.lang.Stemmer;
+
+public class CaStemmer implements Stemmer {
+
+	@Override
+	public List<String> stemListWords(List<String> listWords) {
+
+		List<String> stemmedLW = new ArrayList<String>();
+		
+		for (String word: listWords)
+			stemmedLW.add(stemWord(word));
+		
+		return stemmedLW;
+	}
+	
+	
+	public static String stemWord (String word){
+		
+		CatalanStemmer stemmer = new CatalanStemmer();
+		
+		stemmer.setCurrent(word);
+		stemmer.stem();
+		return stemmer.getCurrent();
+	}
+	
+	public static void main(String[] args) {
+		Stemmer Stemmer=new CaStemmer();
+		List<String> tstList = new ArrayList<String>();
+		tstList.add("jordan");
+		tstList.add("va");
+		tstList.add("rebre");
+		tstList.add("una");
+		tstList.add("beca");
+		tstList.add("per");
+		tstList.add("jugar");
+		tstList.add("a");
+		tstList.add("bàsquet");
+		tstList.add("a");
+		tstList.add("la");
+		tstList.add("universitat");
+		tstList.add("de");
+		tstList.add("carolina");
+		tstList.add("del");
+		tstList.add("nord");
+		tstList.add("on");
+		tstList.add("s");
+		tstList.add("especialitzà");
+		tstList.add("en");
+		tstList.add("geografia");
+	
+		tstList = Stemmer.stemListWords(tstList);
+		System.out.println(tstList.toString());	
+
+	}
+	
+	
+}
+
+
+
+
+
