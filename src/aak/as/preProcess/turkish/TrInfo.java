907febabcc83ae165f5adf096279d72f027d5b5d
Abdelkrime Aries <kariminfo0@gmail.com>
2015-01-23 20:50:13 +0100

    transform turkish to version 0.6.0

---
+++ src/aak/as/preProcess/turkish/TrInfo.java
@@ -0,0 +1,27 @@
+package aak.as.preProcess.turkish;
+
+import aak.as.preProcess.lang.PreProcessInfo;
+
+public class TrInfo implements PreProcessInfo {
+
+	@Override
+	public String getISO639_1() {
+		return "tr";
+	}
+
+	@Override
+	public String getLangEnglishName() {
+		return "Turkish";
+	}
+
+	@Override
+	public String getLangName() {
+		return "Türkçe";
+	}
+
+	@Override
+	public String getPrefix() {
+		return "Tr";
+	}
+
+}
