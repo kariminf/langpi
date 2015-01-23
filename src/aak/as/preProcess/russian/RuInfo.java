1aa73c7a7683235ae151f4efc8fcd4d144fd5712
Abdelkrime Aries <kariminfo0@gmail.com>
2015-01-23 20:35:49 +0100

    transform russian to version 0.6.0

---
+++ src/aak/as/preProcess/russian/RuInfo.java
@@ -0,0 +1,27 @@
+package aak.as.preProcess.russian;
+
+import aak.as.preProcess.lang.PreProcessInfo;
+
+public class RuInfo implements PreProcessInfo {
+
+	@Override
+	public String getISO639_1() {
+		return "ru";
+	}
+
+	@Override
+	public String getLangEnglishName() {
+		return "Russian";
+	}
+
+	@Override
+	public String getLangName() {
+		return "Русский";
+	}
+
+	@Override
+	public String getPrefix() {
+		return "Ru";
+	}
+
+}
