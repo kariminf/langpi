4ec3f70680e396d650405d6df1ca79ea4ccd8460
Abdelkrime Aries <kariminfo0@gmail.com>
2015-02-10 11:08:39 +0100

    add thai metainf

---
+++ src/aak/as/preProcess/thai/ThInfo.java
@@ -0,0 +1,27 @@
+package aak.as.preProcess.thai;
+
+import aak.as.preProcess.lang.PreProcessInfo;
+
+public class ThInfo implements PreProcessInfo {
+
+	@Override
+	public String getISO639_1() {
+		return "th";
+	}
+
+	@Override
+	public String getLangEnglishName() {
+		return "Thai";
+	}
+
+	@Override
+	public String getLangName() {
+		return "";
+	}
+
+	@Override
+	public String getPrefix() {
+		return "Th";
+	}
+
+}
